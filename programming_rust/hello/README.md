067bc86c2a8221805880d7b4553002da3e8eb3e2
Yuta Mizui <me@mziyut.com>
2022-11-25 10:00:34 +0900

    Programming rust

---
+++ programming_rust/hello/README.md
@@ -0,0 +1,48 @@
+# Programing rust / hello
+
+## Log
+
+### Enviroments
+
+```sh
+> cargo --version 
+cargo 1.65.0 (4bc8f24d3 2022-10-20)
+> rustc --version
+rustc 1.65.0 (897e37553 2022-11-02)
+> rustdoc --version
+rustdoc 1.65.0 (897e37553 2022-11-02)
+```
+
+### Create project
+
+```sh
+> cargo new hello
+```
+
+### Directory structure
+
+```sh
+> tree .
+.
+├── Cargo.lock
+├── Cargo.toml
+├── README.md
+└── src
+    └── main.rs
+
+1 directory, 4 files
+```
+
+## How to build, test
+
+```
+> cargo run
+> cargo test
+> cargo fmt
+```
+
+## References
+
+- [rustup.rs - The Rust toolchain installer](https://rustup.rs/)
+- [Rust Programming Language](https://www.rust-lang.org/)
+- [rust-lang/rustfmt: Format Rust code](https://github.com/rust-lang/rustfmt)
---
+++ programming_rust/hello/src/main.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::str::FromStr;
+
+fn main() {
+    let mut num = Vec::new();
+
+    for arg in env::args().skip(1) {
+        num.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if num.len() == 0 {
+        eprintln!("Usage: gcd NUMBER...");
+        std::process::exit(1);
+    }
+
+    let mut d = num[0];
+    for m in &num[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", num, d);
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
